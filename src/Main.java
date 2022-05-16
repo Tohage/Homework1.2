5222b60c12941b1030b4e56e46af8a7f03189a37
tohage <avgorin89@gmail.com>
2022-05-16 19:10:58 +0300

    finalized3

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Расчет колличества миль за купленные билеты:");
-        int priceticket = 5000;
-        int byetickets = 5;
+        int priceTicket = 10_000;
+        int byeTickets = 1;
         int price = 20;
-        int miles = (priceticket / price) * byetickets;
-        System.out.println("Стоимость билета - " + priceticket);
-        System.out.println("Куплено билетов - " + byetickets);
+        int miles = (priceTicket / price) * byeTickets;
+        System.out.println("Стоимость билета - " + priceTicket);
+        System.out.println("Куплено билетов - " + byeTickets);
         System.out.println("Начислено миль - " + miles);
     }
 }
